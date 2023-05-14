35f190d4c03f0a99f98c2a8068c3f3f4aca11cf6
fazlay26 <rabboi007@gmail.com>
2023-05-14 14:37:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState('');
+  console.log(search);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input onChange={(e) => { setSearch(e.target.value) }} type="text" placeholder="Search contacts" className="input input-bordered w-full max-w-xs mt-5" />
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>First_Name</th>
+              <th>Last_Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.filter((d) => {
+                return search.toLowerCase() === '' ? d : d.first_name.toLowerCase().includes(search);
+              }).map(d => <>
+                <tr key={d.id}>
+                  <th>{d.id}</th>
+                  <td>{d.first_name}</td>
+                  <td>{d.last_name}</td>
+                  <td>{d.email}</td>
+                  <td>{d.phone}</td>
+                </tr>
+
+              </>)
+            }
+            {/* row 1 */}
+
+
+
+          </tbody>
+        </table>
+      </div>
+
+
     </div>
   );
 }
